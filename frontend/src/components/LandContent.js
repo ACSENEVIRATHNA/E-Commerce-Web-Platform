666fa4b8b005aa2132904ca6947d8a68c25530a3
Amanda Charith Senevirathna <131018593+ACSENEVIRATHNA@users.noreply.github.com>
2024-04-22 16:09:26 +0530

    Final Project

--- frontend/src/components/LandContent.js
+++ frontend/src/components/LandContent.js
@@ -5,13 +5,15 @@ const LandContent = ({ product }) => {
     <>
       <div className="product col-2">
         {console.log(product.image)}
-        {product.image && (
-        <img
-        className="pro-image"
-        alt={product.name}
-        src={`http://localhost:4000/api/public/image/${product._id}`}
-      />
-      )}
+        <div className="img-container">
+          {product.image && (
+            <img
+              className="pro-image"
+              alt={product.name}
+              src={`http://localhost:4000/api/public/image/${product._id}`}
+            />
+          )}
+        </div>
         <div className="name">{product.name}</div>
         <p className="desc">{product.description}</p>
         <div className="price">Rs. {product.price}</div>
