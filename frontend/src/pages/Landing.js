699e020894641db0704dc6a974b6c6a13d0175ac
Amanda Charith Senevirathna <131018593+ACSENEVIRATHNA@users.noreply.github.com>
2024-06-28 01:49:32 +0530

    update the backend url.js

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -9,7 +9,7 @@ const Landing = () => {
   const { products, dispatch } = useProductContext();
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch("https://chama-computers.onrender.com/api/public");
+      const response = await fetch("http://13.48.55.164/api/public");
       const json = await response.json();
 
       if (response.ok) {
